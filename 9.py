65c25ea956af527a51c461b807d5abfcff31f102
Elijah Kin <3limkin@gmail.com>
2022-04-07 01:49:06 -0400

    Added 9.py

---
+++ 9.py
@@ -0,0 +1,9 @@
+# a^2+b^2=c^2 and a+b+c=1000 --> b=(1000^2-2000a)/(2000-2a)
+
+for a in range(1000):
+    b = (1000*1000-2000*a) // (2000-2*a)
+    c = 1000 - a - b
+    if a > 0 and b > 0 and c > 0:
+        if a*a+b*b == c*c:
+            break
+print(a*b*c)
